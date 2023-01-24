0f995f5cab54bf52de3cc8dc025a54913a8d7795
Suresh G <sureshg.g@gmail.com>
2023-01-23 21:05:12 -0800

    chore: native-image kotlin configurations fix

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ import org.jetbrains.kotlin.gradle.dsl.JvmTarget.*
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-  id("org.springframework.boot") version "3.0.1"
+  id("org.springframework.boot") version "3.0.2"
   id("io.spring.dependency-management") version "1.1.0"
   id("org.graalvm.buildtools.native") version "0.9.19"
   kotlin("jvm") version "1.8.0"
--- src/main/kotlin/dev/suresh/aot/AotConfig.kt
+++ src/main/kotlin/dev/suresh/aot/AotConfig.kt
@@ -1,6 +1,5 @@
 package dev.suresh.aot
 
-import dev.suresh.controller.GreetingsController
 import dev.suresh.data.JavaRec
 import dev.suresh.data.KotlinData
 import org.springframework.aot.hint.MemberCategory
@@ -16,9 +15,7 @@ class Hints : RuntimeHintsRegistrar {
     hints.reflection().apply {
       registerType(JavaRec::class.java, *MemberCategory.values())
       registerType(KotlinData::class.java, *MemberCategory.values())
-      // Requires until we have fix for
-      // https://github.com/spring-projects/spring-framework/issues/29663
-      registerType(GreetingsController::class.java, MemberCategory.INVOKE_DECLARED_METHODS)
+      // registerType(GreetingsController::class.java, MemberCategory.INVOKE_DECLARED_METHODS)
     }
   }
 }
